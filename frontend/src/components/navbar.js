c17d84e700dcd5b2136957575dbb40d8d92947d2
unknown-dev-257 <unknownforeveryone257@gmail.com>
2024-03-14 10:41:40 +0530

    basic navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,5 +1,60 @@
 import React from "react";
+import logo from "../Images/orbit-logo.png";
 
 export default function navbar() {
-  return <div></div>;
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary p-3">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img
+              src={logo}
+              alt="Logo"
+              width="60"
+              height="50"
+              className="d-inline-block align-text-top"
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-center"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Courses
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-disabled="true">
+                  About Us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
